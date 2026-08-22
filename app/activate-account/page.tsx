98f1e4d5800e8ebbe823ee016ba7fe7622c112ed
aditya <aditya@gmail.com>
2026-08-23 02:09:03 +0530

    Add secure legacy account activation

---
+++ app/activate-account/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "../AuthForm";
+
+export default function ActivateAccount() {
+  return <AuthForm mode="activate" />;
+}
---
+++ app/api/auth/claim-legacy/route.ts
@@ -0,0 +1,3 @@
+import { claimLegacyAccount } from "../_service";
+
+export const POST = claimLegacyAccount;
